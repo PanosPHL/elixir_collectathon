dc8a218267378f42a0e7cd01f616d4fc6edb243c
Panayiotis Dimopoulos <panayiotis.dimopoulos@thinkcompany.com>
2025-11-14 13:49:00 -0500

    feat(game): updated player start position and color based on when they joined

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,29 +1,31 @@
 export default {
-  mounted() {
-    const canvas = this.el;
-    const ctx = canvas.getContext('2d');
-    let state = { players: {}, current_letter: null };
-    const boxLengthWidth = 40;
+	mounted() {
+		const canvas = this.el;
+		const ctx = canvas.getContext('2d');
+		let state = { players: {}, current_letter: null };
+		const boxLengthWidth = 40;
 
-    this.handleEvent('game_update', (newState) => {
-      console.log(newState);
-      state = newState;
-    });
+		this.handleEvent('game_update', (newState) => {
+			console.log(newState);
+			state = newState;
+		});
 
-    const draw = () => {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
+		const draw = () => {
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-      for (const id in state.players) {
-        const player = state.players[id];
-        const [playerX, playerY] = player.position;
+			for (const id in state.players) {
+				const player = state.players[id];
+				const [playerX, playerY] = player.position;
 
-        ctx.fillStyle = 'skyblue';
-        ctx.fillRect(playerX, playerY, boxLengthWidth, boxLengthWidth);
-      }
+				console.log(player.position);
 
-      requestAnimationFrame(draw);
-    };
+				ctx.fillStyle = player.color;
+				ctx.fillRect(playerX, playerY, boxLengthWidth, boxLengthWidth);
+			}
 
-    draw();
-  },
+			requestAnimationFrame(draw);
+		};
+
+		draw();
+	},
 };
--- lib/elixir_collectathon/players/player.ex
+++ lib/elixir_collectathon/players/player.ex
@@ -1,14 +1,42 @@
 defmodule ElixirCollectathon.Players.Player do
+  alias ElixirCollectathon.Games.Game
   alias __MODULE__
 
-  @derive Jason.Encoder
-  defstruct name: "", position: {0, 0}, velocity: {0, 0}, inventory: ""
+  @player_lw 40
 
-  def new(name) do
-    %Player{name: name}
+  @player_colors %{
+    1 => "red",
+    2 => "blue",
+    3 => "yellow",
+    4 => "green"
+  }
+
+  @derive Jason.Encoder
+  defstruct color: "red", name: "", position: {0, 0}, velocity: {0, 0}, inventory: ""
+
+  def new(name, player_num \\ 1) do
+    {map_x, map_y} = Game.get_map_size()
+
+    position =
+      case player_num do
+        1 -> {0, 0}
+        2 -> {map_x - @player_lw, 0}
+        3 -> {0, map_y - @player_lw}
+        4 -> {map_x - @player_lw, map_y - @player_lw}
+      end
+
+    %Player{
+      name: name,
+      color: @player_colors[player_num],
+      position: position
+    }
   end
 
   def set_velocity(%Player{} = player, velocity) do
     %Player{player | velocity: velocity}
+  end
+
+  def set_position(%Player{} = player, position) do
+    %Player{player | position: position}
   end
 end
